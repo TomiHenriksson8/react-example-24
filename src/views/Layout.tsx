b4386ba1774358d0047cafaeb0f759b3f0747d6c
TomiHenriksson8 <tomihen01@gmail.com>
2024-02-02 18:52:25 +0200

    add routing

---
+++ src/views/Layout.tsx
@@ -0,0 +1,31 @@
+import {Link, Outlet} from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/upload">Upload</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <p>Copyright 2024 - NN</p>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
